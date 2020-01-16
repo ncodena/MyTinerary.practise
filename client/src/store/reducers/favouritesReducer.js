e49f6915a0bef5258ee64709ad1b54aa3a81128e
Nuria <c.odena.nuria@gmail.com>
2020-01-16 16:18:27 +0100

    favourites reducer created

---
+++ client/src/store/reducers/favouritesReducer.js
@@ -0,0 +1,35 @@
+import {REQUEST_FAVOURITES} from '../actions/favouritesAction';
+import {RECEIVE_FAVOURITES} from '../actions/favouritesAction';
+import {FAILURE_FETCHING_FAVOURITES} from '../actions/favouritesAction';
+
+const initialState = {
+    loading: false,
+    favourites: []
+}
+
+export default function reducer (state = initialState, action) {
+    switch(action.type) {
+        case REQUEST_FAVOURITES:
+            return {
+               ...state,
+               loading:true
+
+            }
+        case RECEIVE_FAVOURITES:
+            return {
+                ...state,
+                loading: false,
+                cities: action.favourites,
+
+            }
+        case FAILURE_FETCHING_FAVOURITES:
+            return {
+                ...state,
+              loading: false,
+              error: action.error
+
+            }
+            default:
+                return state
+    }
+}
