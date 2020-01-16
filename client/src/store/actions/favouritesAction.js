7e1dfd4f95cb1bcfe3575de2913d0b1d2fca922b
Nuria <c.odena.nuria@gmail.com>
2020-01-16 16:09:07 +0100

    favourites actions created

---
+++ client/src/store/actions/favouritesAction.js
@@ -0,0 +1,40 @@
+export const REQUEST_FAVOURITES = 'REQUEST_FAVOURITES'
+export const RECEIVE_FAVOURITES = 'RECEIVE_FAVOURITES'
+export const FAILURE_FETCHING_FAVOURITES = 'FAILURE_FETCHING_FAVOURITES'
+
+const requestFavourites = () => {
+    return{
+        type: REQUEST_FAVOURITES
+        
+    }   
+}
+
+const receiveFavourites = favourites => {
+    return{
+        type: RECEIVE_FAVOURITES,
+        favourites
+        
+    }   
+}
+
+const failureGettingFavourites = error => {
+    return{
+        type: FAILURE_FETCHING_FAVOURITES,
+        error
+        
+    }   
+}
+
+export function fetchFavourites() {
+    return function (dispatch) {
+        dispatch(requestFavourites())
+        return fetch('http://localhost:5000/favourites/all')
+            .then(response => response.json()) 
+            .then (json => {
+                dispatch(receiveFavourites(json)) 
+            })
+            .catch(error => {
+                dispatch(failureGettingFavourites(error.message))
+            })
+    }
+}
